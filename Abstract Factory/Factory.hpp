360ca854ec30e0e1e512a68621df6bf68a62825a
Yinghong Liao <382112699@qq.com>
2016-04-29 21:38:09 +0800

    Added files via upload

---
+++ Abstract Factory/Factory.hpp
@@ -0,0 +1,61 @@
+﻿//
+//  Factory.hpp
+//  polymorphic and OOP design pattern
+//  Abstract Factory
+//
+//  Created by Nicholas.L.Y on 2016/4/25.
+//  Copyright © 2016年 廖颖泓. All rights reserved.
+//
+class Window;
+class UIFactory {
+public:
+    virtual ~UIFactory() = 0;
+    virtual ToolboxWindow getToolboxWindow() = 0;
+    virtual LayersWindow getLayersWindow() = 0;
+    virtual MainWindow getMainWindow() = 0;
+protected:
+    UIFactory();
+};
+class MFCUIFactory : public UIFactory {
+public:
+    MFCUIFactory();
+    ~MFCUIFactory();
+    ToolboxWindow* getToolboxWindow() {
+        return new ToolboxWindow();
+    }
+    LayersWindow* getLayersWindow() {
+        return new LayersWindow();
+    }
+    MainWindow* getMainWindow() {
+        return new MainWindow();
+    }
+};
+class GtkUIFactory : public UIFactory {
+public:
+    GtkUIFactory();
+    ~GtkUIFactory();
+    ToolboxWindow* getToolboxWindow() {
+        return new ToolboxWindow();
+    }
+    LayersWindow* getLayersWindow() {
+        return new LayersWindow();
+    }
+    MainWindow* getMainWindow() {
+        return new MainWindow();
+    }
+};
+class QtUIFactory : public UIFactory {
+public:
+    QtUIFactory();
+    ~QtUIFactory();
+    ToolboxWindow* getToolboxWindow() {
+        return new ToolboxWindow();
+    }
+    LayersWindow* getLayersWindow() {
+        return new LayersWindow();
+    }
+    MainWindow* getMainWindow() {
+        return new MainWindow();
+    }
+};
+
---
+++ Abstract Factory/Product.hpp
@@ -0,0 +1,38 @@
+﻿//
+//  Product.hpp
+//  polymorphic and OOP design pattern
+//  Abstract Factory
+//
+//  Created by Nicholas.L.Y on 2016/4/25.
+//  Copyright © 2016年 廖颖泓. All rights reserved.
+//
+class Window {
+public:
+    virtual ~Window() = 0;
+    virtual void operation() = 0;
+    virtual getToolkit() = 0;
+    virtual getType() = 0;
+    void deleteWindow(Window&);
+protected:
+    Window();
+
+};
+class ToolboxWindow : public Window {
+public:
+    ToolboxWindow();
+    virtual void operation();
+    virtual ~ToolboxWindow();
+};
+class LayersWindow : public Window {
+public:
+    LayersWindow();
+    virtual void operation();
+    virtual ~LayersWindow();
+};
+class MainWindow : public Window {
+public:
+    MainWindow();
+    virtual void operation();
+    virtual ~MainWindow();
+};
+
